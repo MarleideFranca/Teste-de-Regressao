9932d8cbc0070f4e2a36b21b13196574c971463e
MarleideFranca <marleidealves2002@yahoo.com.br>
2025-07-10 06:11:50 -0300

    Implementando a classe TrianguloTest

--- teste-de-regressao/src/test/java/marleidealves/TrianguloTest.java
+++ teste-de-regressao/src/test/java/marleidealves/TrianguloTest.java
@@ -40,4 +40,34 @@ public class TrianguloTest {
         Triangulo t = new Triangulo(2, 2, 4);
         assertEquals("Triângulo inválido", t.classificarTriangulo());
     }
+
+    @Test
+    public void testCalcularPerimetroValido() {
+        Triangulo t = new Triangulo(3.0, 4.0, 5.0); // triângulo escaleno válido
+        double esperado = 12.0;
+        assertEquals(esperado, t.calculaPerimetro(), 0.0001);
+    }
+
+     @Test
+    public void testCalcularPerimetroEquilatero() {
+        Triangulo t = new Triangulo(5.0, 5.0, 5.0); // triângulo equilátero
+        double esperado = 15.0;
+        assertEquals(esperado, t.calculaPerimetro(), 0.0001);
+    }
+
+    @Test
+    public void testCalcularPerimetroIsosceles() {
+        Triangulo t = new Triangulo(5.0, 5.0, 3.0); // triângulo isósceles
+        double esperado = 13.0;
+        assertEquals(esperado, t.calculaPerimetro(), 0.0001);
+    }
+
+     @Test
+    public void testPerimetroTrianguloInvalido() {
+        Triangulo t = new Triangulo(1.0, 2.0, 10.0); // não satisfaz a desigualdade triangular
+        Exception excecao = assertThrows(IllegalStateException.class, () -> {
+            t.calculaPerimetro();
+        });
+        assertEquals("Não é possível calcular o perímetro de um triângulo inválido.", excecao.getMessage());
+    }
 }
