94cb4f65e4c030fd285c8b034da20af00f4c85a6
MarleideFranca <marleidealves2002@yahoo.com.br>
2025-07-11 11:58:36 -0300

    Implementando a classe Veiculo no projeto oficina

---
+++ oficina/src/main/java/marleidealves/Veiculo.java
@@ -0,0 +1,66 @@
+package marleidealves;
+
+public class Veiculo {
+
+    // Atributos
+    private String placa;
+    private String tipo;
+    private int numOcorrencias;
+    private int anoFabricacao;
+
+    // Construtor
+    public Veiculo(String placa, String tipo, int numOcorrencias, int anoFabricacao) {
+        this.placa = placa;
+        this.tipo = tipo.toUpperCase(); // normaliza para facilitar a verificação
+        this.numOcorrencias = numOcorrencias;
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    // Getters e Setters
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo.toUpperCase();
+    }
+
+    public int getNumOcorrencias() {
+        return numOcorrencias;
+    }
+
+    public void setNumOcorrencias(int numOcorrencias) {
+        this.numOcorrencias = numOcorrencias;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    // Método que calcula o ano de expiração da garantia
+    public int calculaGarantia() {
+        switch (tipo) {
+            case "PICKUP":
+                return anoFabricacao + 5;
+            case "SUV":
+                return anoFabricacao + 3;
+            case "POPULAR":
+                return anoFabricacao + 2;
+            default:
+                // Se o tipo não for reconhecido, considera sem garantia
+                return anoFabricacao;
+        }
+    }
+}
